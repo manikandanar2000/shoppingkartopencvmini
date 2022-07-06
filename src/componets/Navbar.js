d82546f83a73d73aa3f8d61ab448742907cd0a0b
manikandanar2000 <mani4.reghu@gmail.com>
2022-07-06 22:28:39 +0530

    first nav

---
+++ src/componets/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css';
+function Navbar() {
+  return (
+    <div className='nav'>
+       <div className='nav2'>
+        
+
+       </div>
+       <div className='nav3'>
+
+       <table>
+            <tr>
+                <td className='log'>Logout</td>
+            </tr>
+         </table>
+
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
